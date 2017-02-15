a94c47adb4d0c9e4ab443bc8984b18d0255342d5
Lukas Cech <ja@lukascech.cz>
2017-02-15 09:51:27 +0100

    Initial Commit

---
+++ js/utag.footer.js
@@ -0,0 +1,1 @@
+//tealium universal tag - utag.footer ut4.0.201702150824, Copyright 2017 Tealium.com Inc. All Rights Reserved.
---
+++ js/utag.sync.js
@@ -0,0 +1,1 @@
+//tealium universal tag - utag.sync ut4.0.201702150824, Copyright 2017 Tealium.com Inc. All Rights Reserved.
